a41103bcfd12b16b4e8b00d59a935244b4d978a7
zoray <laikitasony@gmail.com>
2024-10-26 23:39:18 -0500

    svm 2

--- Modelo_SVM.py
+++ Modelo_SVM.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.svm import SVC
@@ -10,57 +11,59 @@ from sklearn.pipeline import Pipeline
 
 class ModeloSVM:
     def __init__(self, datos, objetivo):
+        """
+        Inicializa la clase ModeloSVM con datos y objetivo.
+
+        Parámetros:
+        - datos: DataFrame con las características.
+        - objetivo: Series o array con la variable de salida.
+        """
         self.datos = datos
         self.objetivo = objetivo
         self.X_entrenamiento, self.X_prueba, self.y_entrenamiento, self.y_prueba = train_test_split(
             datos, objetivo, test_size=0.2, random_state=42
         )
 
-    def limpieza(self):
-        for col in self.X_entrenamiento.select_dtypes(include=[np.number]).columns:
-            self.X_entrenamiento[col].fillna(self.X_entrenamiento[col].mean(), inplace=True)
-            self.X_prueba[col].fillna(self.X_prueba[col].mean(), inplace=True)
-
-        # Rellenar valores faltantes con la moda para columnas categóricas
-        for col in self.X_entrenamiento.select_dtypes(include=[object]).columns:
-            self.X_entrenamiento[col].fillna(self.X_entrenamiento[col].mode()[0], inplace=True)
-            self.X_prueba[col].fillna(self.X_prueba[col].mode()[0], inplace=True)
-
-    def transformaciones(self):
-        # Transformar características categóricas con OneHotEncoder y estandarizar características numéricas
-        caracteristicas_numericas = ['caracteristica_numerica1', 'caracteristica_numerica2']
-        caracteristicas_categoricas = ['caracteristica_categorica']
-
-        self.preprocesador = ColumnTransformer(
-            transformers=[
-                ('num', StandardScaler(), caracteristicas_numericas),
-                ('cat', OneHotEncoder(), caracteristicas_categoricas)
-            ]
-        )
-
-        self.X_entrenamiento = self.preprocesador.fit_transform(self.X_entrenamiento)
-        self.X_prueba = self.preprocesador.transform(self.X_prueba)
-
-    def preprocesar(self):
-        self.limpieza()
-        self.transformaciones()
-
     def entrenar(self):
+        """
+        Entrena el modelo SVM utilizando GridSearchCV para optimizar los hiperparámetros.
+        """
+        # Definimos los hiperparámetros que queremos optimizar
         param_grid = {
             'C': [0.1, 1, 10],
             'kernel': ['linear', 'rbf', 'poly']
         }
 
+        # Creamos el clasificador SVM
         svm = SVC()
+
+        # Utilizamos GridSearchCV para encontrar la mejor combinación de hiperparámetros
         self.busqueda_grid = GridSearchCV(svm, param_grid, cv=3, scoring='accuracy')
         self.busqueda_grid.fit(self.X_entrenamiento, self.y_entrenamiento)
 
     def evaluar(self):
+        """
+        Evalúa el modelo SVM en el conjunto de prueba y devuelve la precisión.
+
+        Retorna:
+        - exactitud: Precisión del modelo en el conjunto de prueba.
+        """
+        # Realizamos predicciones en el conjunto de prueba
         y_pred = self.busqueda_grid.predict(self.X_prueba)
+
+        # Calculamos la exactitud
         exactitud = accuracy_score(self.y_prueba, y_pred)
         return exactitud
 
     def predecir(self, nuevos_datos):
-        nuevos_datos_transformados = self.preprocesador.transform(nuevos_datos)
-        predicciones = self.busqueda_grid.predict(nuevos_datos_transformados)
+        """
+        Realiza predicciones en nuevos datos.
+
+        Parámetros:
+        - nuevos_datos: DataFrame o array con los datos para predecir.
+
+        Retorna:
+        - predicciones: Array con las predicciones del modelo.
+        """
+        predicciones = self.busqueda_grid.predict(nuevos_datos)
         return predicciones
