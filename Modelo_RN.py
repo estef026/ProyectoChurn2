bc4db929b5f1a054023c99efd1e79ad5ecb25ee9
estef026 <estefania.castillo212@gmail.com>
2024-10-30 17:42:59 -0500

    Estefania 17

--- Modelo_RN.py
+++ Modelo_RN.py
@@ -52,4 +52,3 @@ class ModeloRedNeuronal:
     def classification_report(self, y_test, y_pred):
         report = classification_report(y_test, y_pred)
         print("Informe de Clasificación:\n", report)
-        return report
