b1d1b0c091bf53883ff4ce631299bdee0802357a
estef026 <estefania.castillo212@gmail.com>
2024-11-04 17:26:38 -0500

    Estefania 20

--- Grid_Search_NN.py
+++ Grid_Search_NN.py
@@ -22,4 +22,16 @@ def create_model(input_shape = 43, optimizer = 'adam'):
     model.summary()
     return model
 
-def grid_search_nn():
+def grid_search_nn(estimator, scoring, cv, X_train, y_train):
+
+    grid_search = GridSearchCV(estimator= estimator,
+                               param_grid={'batch_size': [32, 64, 128],
+                                            'epochs': [5, 10, 15],
+                                            'optimizer': ['adam', 'rmsprop'],
+                                            'optimizer__learning_rate': [0.001, 0.01, 0.1]},
+                               scoring= scoring,
+                               cv=cv)
+
+    grid_search.fit(X_train, y_train, verbose=0)
+
+    return grid_search.best_params_, grid_search.best_score_
