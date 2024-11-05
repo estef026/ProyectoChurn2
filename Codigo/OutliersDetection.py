7129a34b3bc89d5c4f5bc09e4085d38c190f16c4
estef026 <estefania.castillo212@gmail.com>
2024-11-04 20:17:37 -0500

    Estefania 20

--- Codigo/OutliersDetection.py
+++ Codigo/OutliersDetection.py
@@ -6,6 +6,8 @@ class OutlierDetector:
 
     def count_outliers(self, column):
 
+
+
         Q1 = self.df[column].quantile(0.25)
         Q3 = self.df[column].quantile(0.75)
 
@@ -15,4 +17,15 @@ class OutlierDetector:
         upper_bound = Q3 + 1.5 * IQR
 
         outliers = self.df[(self.df[column] < lower_bound) | (self.df[column] > upper_bound)]
+
         return len(outliers)
+
+    def outliers_summary(self):
+
+        outlier_counts = {}
+        for column in self.df.select_dtypes(include=['float', 'int']).columns:
+            outlier_counts[column] = self.count_outliers(column)
+
+        # Convert dictionary to DataFrame
+        outliers_df = pd.DataFrame(outlier_counts.items(), columns=['Column', 'Outlier_Count'])
+        return outliers_df
